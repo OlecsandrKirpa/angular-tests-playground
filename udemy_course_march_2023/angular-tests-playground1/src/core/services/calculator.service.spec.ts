2244dbdedfa3b8bc6550644c5964371493cc8033
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-06 11:23:39 +0100

    NEW basic createSpyObj example

--- udemy_course_march_2023/angular-tests-playground1/src/core/services/calculator.service.spec.ts
+++ udemy_course_march_2023/angular-tests-playground1/src/core/services/calculator.service.spec.ts
@@ -1,27 +1,66 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
-  let service: CalculatorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+  it('spyOn logger.log', () => {
+    const logger1 = new LoggerService();
+
+    spyOn(logger1, 'log');
+
+    const service1 = new CalculatorService(logger1);
+
+    service1.add(1, 2);
+    expect(logger1.log).toHaveBeenCalledWith('add', [1, 2]);
+    expect(logger1.log).toHaveBeenCalledTimes(1)
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-    expect(service instanceof CalculatorService).toBe(true);
+  it('jasmine.createSpyObj - logger.log', () => {
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+    const service = new CalculatorService(logger);
+
+    service.add(1, 2);
+    expect(logger.log).toHaveBeenCalledWith('add', [1, 2]);
   });
 
-  it('should add two numbers', () => {
-    // pending('gigi');
-    expect(service.add(1, 2)).toBe(3);
+  describe('with beforeEach', () => {
+    let service: CalculatorService;
+    let logger: LoggerService;
+
+    beforeEach(() => {
+      logger = new LoggerService();
+
+      spyOn(logger, 'log');
+
+      service = new CalculatorService(
+        logger
+      );
+
+      // TestBed.configureTestingModule({});
+      // service = TestBed.inject(CalculatorService);
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+      expect(service instanceof CalculatorService).toBe(true);
+    });
+
+    it('should add two numbers', () => {
+      // pending('gigi');
+      expect(service.add(1, 2)).toBe(3);
+      expect(logger.log).toHaveBeenCalledWith('add', [1, 2]);
+      expect(logger.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers', () => {
+      // fail("something went wrong");
+      expect(service.subtract(2, 1)).toBe(1, '<custom message>');
+      expect(logger.log).toHaveBeenCalledWith('subtract', [2, 1]);
+      expect(logger.log).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should subtract two numbers', () => {
-    // fail("something went wrong");
-    expect(service.subtract(2, 1)).toBe(1, '<custom message>');
-  });
 });

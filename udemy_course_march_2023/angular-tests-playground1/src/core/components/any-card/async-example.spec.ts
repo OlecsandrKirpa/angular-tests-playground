5498352433c36d6eaea43d4129510752e7140639
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-07 12:03:45 +0100

    fakeasync, promises, macrotasks, flush, tick

--- udemy_course_march_2023/angular-tests-playground1/src/core/components/any-card/async-example.spec.ts
+++ udemy_course_march_2023/angular-tests-playground1/src/core/components/any-card/async-example.spec.ts
@@ -15,13 +15,13 @@ describe('async texamples', () => {
     let test = false;
 
     setTimeout(() => {
-      console.log('test 1');
+      // console.log('test 1');
     })
 
     setTimeout(() =>
       setTimeout(() => {
         test = true;
-        console.log('test 2');
+        // console.log('test 2');
       })
       , 1000);
 
@@ -30,6 +30,59 @@ describe('async texamples', () => {
     // tick(1000);
 
     expect(test).toBeTruthy();
+  }));
+
+  it('async test example - plain Promise', fakeAsync(() => {
+    let test = false;
+
+    console.log('Creating promise');
+
+    // setTimeout(() => console.log('setTimeout 1 callback called'))
+
+    // setTimeout is a "macrotask" or a "task"
+    // setTimeout(() => console.log('setTimeout 2 callback called'))
+
+    // Promise is a "microtask"
+    Promise.resolve().then(() => {
+      console.log('Promise first then() evaluated successfully');
+      return Promise.resolve();
+    }).then(() => {
+      console.log('Promise second then() evaluated successfully');
+      test = true;
+    })
+
+    flushMicrotasks();
+
+    console.log('End of test spec');
+
+    expect(test).toBeTrue();
+  }));
+
+  it('mix example - plain Promise', fakeAsync(() => {
+    let counter: number = 0;
+    const add = (num: number) => counter += num;
+
+    Promise.resolve().then(() => {
+      add(10);
+
+      setTimeout(() => {
+        add(1);
+      }, 1000)
+    });
+
+    expect(counter).toBe(0);
+
+    flushMicrotasks();
+
+    expect(counter).toBe(10);
+
+    tick(500);
+    
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(11);
   }));
 
   // it('with async/await', async () => {
